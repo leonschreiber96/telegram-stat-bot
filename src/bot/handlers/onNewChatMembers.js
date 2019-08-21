6a74d71991125eb86ea9f7e41164ed673cd85512
Leon Schreiber <leonschreiber96@gmail.com>
2019-08-22 01:31:56 +0200

    fix new chat member handler code style

--- src/bot/handlers/onNewChatMembers.js
+++ src/bot/handlers/onNewChatMembers.js
@@ -5,39 +5,50 @@ import MessageCollection from "../messages/messageCollection";
 import config from "../../../config.json";
 
 export default function on_new_chat_members(message, stat_bot) {
+    if (message.new_chat_members.some(x => x.id == stat_bot.own_id)) {
+        self_enter_chat(message, stat_bot);
+    } else if (message.new_chat_members.some(x => !x.is_bot)) {
+        user_enter_chat(message, stat_bot);
+    }
+}
+
+function self_enter_chat(message, stat_bot) {
     let chat = message.chat.id;
     let bot = stat_bot.bot;
 
-    if (message.new_chat_members.some(x => x.id == stat_bot.own_id)) {
-        let gif = new DocumentMessage(bot, chat, "CgADBAADfQEAAprc5VLEBzPUauGO4hYE");
+    let gif = new DocumentMessage(bot, chat, "CgADBAADfQEAAprc5VLEBzPUauGO4hYE");
 
-        let introduction = new TextMessage(bot, chat, config.language_default, "Markdown");
-        introduction.add_line_translated("introduction_group", { group_name: message.chat.title });
+    let introduction = new TextMessage(bot, chat, config.language_default, "Markdown");
+    introduction.add_line_translated("introduction_group", { group_name: message.chat.title });
 
-        let ask_for_consent = new TextMessage(bot, chat, config.language_default);
-        ask_for_consent.add_line_translated("ask_for_consent_multiple_members");
+    let ask_for_consent = new TextMessage(bot, chat, config.language_default);
+    ask_for_consent.add_line_translated("ask_for_consent_multiple_members");
 
-        let replies = new MessageCollection([
-            gif,
-            introduction,
-            ask_for_consent
-        ]);
+    let replies = new MessageCollection([
+        gif,
+        introduction,
+        ask_for_consent
+    ]);
 
-        replies.send_all();
-    } else if (message.new_chat_members.some(x => !x.is_bot)) {
-        let human_new_members = message.new_chat_members.filter(x => !x.is_bot);
+    replies.send_all();
+}
 
-        let introduction = new TextMessage(bot, chat, config.language_default, "Markdown");
-        introduction.add_line_translated("introduction_new_members", { new_members: human_new_members.map(x => stat_bot.getUserAddress(x)).join(", ") });
+function user_enter_chat(message, stat_bot) {
+    let chat = message.chat.id;
+    let bot = stat_bot.bot;
 
-        let ask_for_consent = new TextMessage(bot, chat, config.language_default, "Markdown");
-        ask_for_consent.add_line_translated(human_new_members.length > 1 ? "ask_for_consent_multiple_members" : "ask_for_consent_single_member");
+    let human_new_members = message.new_chat_members.filter(x => !x.is_bot);
 
-        let replies = new MessageCollection([
-            introduction,
-            ask_for_consent
-        ]);
+    let introduction = new TextMessage(bot, chat, config.language_default, "Markdown");
+    introduction.add_line_translated("introduction_new_members", { new_members: human_new_members.map(x => stat_bot.getUserAddress(x)).join(", ") });
 
-        replies.send_all();
-    }
+    let ask_for_consent = new TextMessage(bot, chat, config.language_default, "Markdown");
+    ask_for_consent.add_line_translated(human_new_members.length > 1 ? "ask_for_consent_multiple_members" : "ask_for_consent_single_member");
+
+    let replies = new MessageCollection([
+        introduction,
+        ask_for_consent
+    ]);
+
+    replies.send_all();
 }
