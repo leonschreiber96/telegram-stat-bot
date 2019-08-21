be59e9bc90474c1fa048bbc2ecadddb16ff176f7
Leon Schreiber <leon.schreiber@omikron.net>
2019-08-21 18:21:27 +0200

    Add config prompts for port & listing
    - Add prompt that asks whether user wants to configure a port
    - Add prompt that configures the port if requested
    - Add prompt that asks whether user wants white/blacklisting
    - Add prompt that configures white/blacklisting if requested

--- src/checkConfig.js
+++ src/checkConfig.js
@@ -12,23 +12,59 @@ if (!config_available) {
     inquirer.prompt([{
         type: "confirm",
         name: "manual_config",
-        message: "Seems like you haven't created a config file for the stat bot yet. We can do that together if you like!",
+        message: "Seems like you haven't created a config file for the stat bot yet. We can do that together now if you like!",
         default: true
     }, {
         type: "password",
         name: "telegram_bot_token",
         message: "What is your telegram bot api token? ",
-        mask: "*"
+        mask: "*",
+        when: answers => answers["manual_config"]
     }, {
         type: "list",
         name: "language_default",
         message: "What language should your bot speak by default? ",
-        choices: ["en", "de"]
+        choices: ["en", "de"],
+        when: answers => answers["manual_config"]
+    }, {
+        type: "list",
+        name: "port_selection",
+        message: "On which port should the backend operate? ",
+        choices: ["Always choose a free one by yourself", "I want to choose my own fixed port"],
+        when: answers => answers["manual_config"]
+    }, {
+        type: "number",
+        name: "port",
+        message: "Please enter your port number: ",
+        when: answers => answers["port_selection"] === "I want to choose my own fixed port"
+    }, {
+        type: "list",
+        name: "white_black_listing",
+        message: "Do you want to control from which chats messages are stored? ",
+        choices: ["Yes, I want a whitelist", "Yes, I want a blacklist", "No, save messages from all chats"],
+        when: answers => answers["manual_config"]
+    }, {
+        type: "input",
+        name: "listed_chats",
+        message: answers => {
+            let list_type = answers["white_black_listing"].substring(answers["white_black_listing"].lastIndexOf(" ") + 1, answers["white_black_listing"].length)
+            return `Please enter the chat you want to include in your ${list_type} as numbers separated by commas. Spaces will be ignored.`
+        },
+        when: answers => answers["white_black_listing"].startsWith("Yes")
     }]).then(answers => {
         if (answers["manual_config"]) {
             config.telegram_bot_token = answers["telegram_bot_token"];
             config.own_id = +config.telegram_bot_token.substring(0, config.telegram_bot_token.indexOf(":"));
             config.language_default = answers["language_default"];
+
+            if (answers["port_selection"] === "I want to choose my own fixed port") {
+                config.port = answers["port"];
+            }
+
+            if (answers["white_black_listing"].startsWith("Yes")) {
+                let list_type = answers["white_black_listing"].substring(answers["white_black_listing"].lastIndexOf(" ") + 1, answers["white_black_listing"].length);
+                config[list_type] = answers["listed_chats"].split(",").map(x => +(x.trim()));
+            }
         } else {
             config.telegram_bot_token = "";
             config.own_id = "";
