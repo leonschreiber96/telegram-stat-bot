a8e8ee7ace6f8aa1869bdba63f844b6800c4f0b8
Leon Schreiber <leonschreiber96@gmail.com>
2019-08-29 01:16:37 +0200

    Improve startup sequence

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 // Import external packages
 import colors from "colors/safe";
 
@@ -7,18 +8,17 @@ import { setup_database, setup_backend, setup_bot } from "./setupFunctions";
 console.log();
 console.log(colors.bgWhite.black("----- Initializing STAT BOT ----------------------------------"));
 
-setup_database()
-    .then(message => {
-        console.log(message);
-        setup_backend().then(message => {
-            console.log(message.message);
-            setup_bot(message.port).then(message => {
-                console.log(message);
-            }).then(() => console.log(colors.bgGreen.black("----- Everything up and running! -----------------------------")));
-        }).catch(error => {
-            console.log(error.message);
-        });
-    }).catch(error => {
+setup();
+
+async function setup() {
+    let setup_database_result = await setup_database();
+    let setup_backend_result = await setup_backend();
+    let setup_bot_result = await setup_bot(setup_backend_result.port);
+
+    if (setup_database_result.success && setup_backend_result.success && setup_bot_result.success) {
+        console.log(colors.bgGreen.black("----- Everything up and running! -----------------------------"));
+    } else {
         console.log(colors.bgRed.black("----- Something went wrong. Check config and try again? ------"));
-        console.error(colors.red(error));
-    });
+        process.exit();
+    }
+}
--- src/setupFunctions.js
+++ src/setupFunctions.js
@@ -11,79 +11,65 @@ import StatBot from "./bot/statBot";
 import config from "../config.json";
 
 export async function setup_database() {
-    return new Promise((resolve, reject) => {
-        try {
-            mongoose.connect("mongodb://localhost/statbottest", { useNewUrlParser: true });
-            var db = mongoose.connection;
-            db.on("error", console.error.bind(console, "connection error:"));
-            db.once("open", function() {
-                resolve(`${colors.green("✔")} connected to database`);
-            });
-        } catch (error) {
-            reject({
-                message: `${colors.red("❌")} An error occurred while setting up MongoDB with Mongoose. Is your connection string correct?`,
-                error: error
-            });
-        }
+    return new Promise((resolve) => {
+        mongoose.connect("mongodb://localhost/statbottest", {
+            useNewUrlParser: true,
+            useFindAndModify: true,
+            useCreateIndex: true
+        });
+
+        var db = mongoose.connection;
+
+        db.on("error", (error) => {
+            if (error.name === "MongoNetworkError") {
+                console.log(`${colors.red("❌")}  Couldn't set up database. Connection didn't succeed.`);
+                resolve({ success: false });
+            } else {
+                console.error.bind(console, "connection error:");
+            }
+        });
+
+        db.once("open", function() {
+            console.log(`${colors.green("✔")} connected to database`);
+            resolve({ success: true });
+        });
     });
 }
 
 export async function setup_backend() {
-    const port = await get_port();
+    const port = config.port || await getPort();
 
-    return new Promise((resolve, reject) => {
-        try {
-            const app = express();
-            app.use(router);
-            app.use(bodyParser.urlencoded({ extended: true }));
-            app.use(bodyParser.json());
+    return new Promise(resolve => {
+        const app = express();
+        app.use(router);
+        app.use(bodyParser.urlencoded({ extended: true }));
+        app.use(bodyParser.json());
 
-            app.listen(port, () => {
-                resolve({
-                    message: `${colors.green("✔")} server running on port ${port}`,
-                    port: port
-                });
-            }).on("error", error => {
+        app.listen(port, () => {
+            console.log(`${colors.green("✔")} server running on port ${port}`);
+            resolve({ success: true, port: port });
+        }).on("error", error => {
+            if (error.code === "EADDRINUSE") {
+                console.log(`${colors.red("❌")}  Couldn't set up backend on port ${port}. Port already in use.`);
+                resolve({ success: false, port: port });
+            } else {
                 console.error("error", error.code);
-            });
-        } catch (error) {
-            reject({
-                message: `${colors.red("❌")} An error occurred while setting up node + express.`,
-                error: error
-            });
-        }
+            }
+        });
+
     });
 }
 
 export async function setup_bot(backend_port) {
-    return new Promise((resolve, reject) => {
-        try {
-            // TODO: handle errors in telegram bot execution
-            const stat_bot = new StatBot(config.telegram_bot_token, config.own_id, backend_port);
-            stat_bot.registerHandlers();
-            resolve(`${colors.green("✔")} Stat bot started and initialized successfully`);
-        } catch (error) {
-            reject({
-                message: `${colors.red("❌")} An error occurred while setting up the telegram bot. Is your token already in use?`,
-                error: error
-            });
-        }
-    });
-}
+    const stat_bot = new StatBot(config.telegram_bot_token, config.own_id, backend_port);
 
-async function get_port() {
     try {
-        let port = config.port || await getPort();
-
-        return new Promise((resolve) => {
-            resolve(port);
-        });
+        await stat_bot.try_start();
+        stat_bot.register_handlers();
+        console.log(`${colors.green("✔")} Stat bot started and initialized successfully`);
+        return { success: true };
     } catch (error) {
-        return new Promise((resolve, reject) => {
-            reject({
-                message: "An error occurred while searching for a free port.",
-                error: error
-            });
-        });
+        console.log(`${colors.red("❌")}  An error occurred while setting up the telegram bot. Is your token invalid or already in use?`);
+        return { success: false };
     }
 }
