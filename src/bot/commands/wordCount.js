086b7bd0be618720aaf23cafaf4f3ad4d1896768
Leon Schreiber <leonschreiber96@gmail.com>
2019-08-29 01:16:08 +0200

    Add word count command

---
+++ src/bot/commands/wordCount.js
@@ -0,0 +1,29 @@
+// Import external packages
+import request from "request-promise";
+
+// Import internal packages
+import TextMessage from "../messages/textMessage";
+
+export default async function word_count(message, stat_bot) {
+    let chat = message.chat.id;
+    let log = ["/word_count"];
+
+    let response = await request({
+        uri: `http://localhost:${stat_bot.backend_port}/messages/wordCount/${chat}`,
+        json: true
+    });
+
+    let result = response.result;
+
+    log.push(result);
+    stat_bot.log(log);
+
+    let reply = new TextMessage(stat_bot.bot, chat, "de", "Markdown");
+
+    reply.add_line_translated("word_count", {
+        total_words: result.total,
+        avgPerMessage: result.avgPerMessage
+    });
+
+    reply.send();
+}
--- src/bot/statBot.js
+++ src/bot/statBot.js
@@ -10,18 +10,29 @@ import on_message from "./handlers/onMessage";
 import on_new_chat_members from "./handlers/onNewChatMembers";
 import total_messages from "./commands/totalMessages";
 import total_messages_extended from "./commands/totalMessagesExtended";
+import word_count from "./commands/wordCount";
 
 export default class StatBot {
     constructor(token, own_id, backend_port) {
-        this.bot = new TelegramBot(token, { polling: true });
+        this.bot = new TelegramBot(token, { autoStart: false });
         this.own_id = own_id;
         this.backend_port = backend_port;
     }
 
-    registerHandlers() {
+    async try_start() {
+        try {
+            let update = await this.bot.getUpdates();
+            await this.bot.processUpdate(update);
+            await this.bot.startPolling();
+        } catch (error) {
+            throw new Error(401);
+        }
+    }
+
+    register_handlers() {
         this.bot.on("message", (message, metadata) => on_message(message, metadata, this));
         this.bot.on("new_chat_members", async message => await on_new_chat_members(message, this));
-        
+
         this.bot.onText(/^\/consent (\w+)$/, (message, match) => consent(message, match[1], this));
         this.bot.onText(/^\/chat_info$/, async message => await chat_info(message, this));
         this.bot.onText(/^\/messages_per_user$/, message => messages_per_user(message, this));
@@ -29,6 +40,7 @@ export default class StatBot {
         // TODO: special text in case there are zero messages yet (e.g. if nobody gave consent yet)
         this.bot.onText(/^\/total_messages$/, message => total_messages(message, this));
         this.bot.onText(/^\/total_messages_extended$/, message => total_messages_extended(message, this));
+        this.bot.onText(/^\/word_count$/, async message => await word_count(message, this));
     }
 
     get_user_address(user) {
