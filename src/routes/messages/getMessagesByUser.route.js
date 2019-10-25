2936422a584d70bb8effbecf7c6253148d5d9790
Leon Schreiber <leon.schreiber@omikron.net>
2019-10-25 16:09:17 +0200

    Add info message that bot is in development for non dev-users

--- src/routes/messages/getMessagesByUser.route.js
+++ src/routes/messages/getMessagesByUser.route.js
@@ -2,13 +2,13 @@ import { get_messages_by_user } from "../../controllers/message.controller";
 
 export default async function get_messages_by_user_route(req, res) {
     let chat_id = parseInt(req.params.chatId);
-    let extended = req.query.extended === "true";
+    // let extended = req.query.extended === "true";
 
     if (isNaN(chat_id)) {
         res.status(400).send("chatId parameter must be an integer");
     }
     try {
-        let result = await get_messages_by_user(chat_id, extended);
+        let result = await get_messages_by_user(chat_id);
         let total_messages = result.map(x => x.count).reduce((sum, value) => sum + value);
 
         for (let i = 0; i < result.length; i++) {
